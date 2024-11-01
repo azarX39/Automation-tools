e4d7e9c10b3b2fd295fdc8ebb7c8154d949255a8
azarX39 <151084710+azarX39@users.noreply.github.com>
2024-11-01 09:01:49 +0800

    Create xss_finder.bash

---
+++ xss_finder.bash
@@ -0,0 +1,18 @@
+# XSS Vulnerability Testing Tool
+
+# This script is designed to automate the testing of web applications for Cross-Site Scripting (XSS) vulnerabilities.
+# It performs the following functions:
+
+# Usage:
+# - Ensure that `subdomains.txt`, `parameters.json`, and `xss_payloads.txt` are prepared with the necessary data.
+# - Execute the script in a Bash environment to perform the tests.
+
+for one_domain in $(cat subdomains.txt); do
+    for one_param in $(cat parameters.txt); do
+        while IFS= read -r one_payload; do
+            curl -s "$one_domain?$one_param=$one_payload" | grep -q "$one_payload" && \
+            echo "There is XSS in $one_param on $one_domain using payload: $one_payload" || \
+            echo "No XSS in $one_param on $one_domain with payload: $one_payload"
+        done < xss_payloads.txt
+    done
+done
